eb87897c382aa5193c05ec23eec0cf448f9e7d26
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2024-08-15 22:10:20 -0300

    refactor tests to use setup

--- mongo_debug_test.go
+++ mongo_debug_test.go
@@ -16,37 +16,13 @@ import (
 
 func TestDebugFindWithoutIndex(t *testing.T) {
 	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/db"))
-	require.NoError(t, err)
+	client, chosenUserName := setup(t, false)
 
-	err = client.Database("db").CreateCollection(ctx, "collection")
-	require.NoError(t, err)
-
-	faker := faker.New()
-
-	randomNumber := rand.IntN(10_000)
-	var name string
-
-	for i := 0; i < 10_000; i++ {
-		userName := faker.Person().Name()
-		if randomNumber == i {
-			name = userName
-		}
-
-		_, err = client.Database("db").Collection("collection").InsertOne(ctx, bson.D{{"name", userName}})
-		require.NoError(t, err)
-	}
-
-	t.Cleanup(func() {
-		err = client.Database("db").Collection("collection").Drop(ctx)
-		require.NoError(t, err)
-	})
-
-	_, err = mongodebug.
+	_, err := mongodebug.
 		Debug(mongodebug.WithThreshold(10)).
 		WithDatabase(client.Database("db")).
 		WithCollection("collection").
-		Find(ctx, bson.D{{"name", name}})
+		Find(ctx, bson.D{{"name", chosenUserName}})
 
 	require.NotNil(t, err)
 	assert.Contains(t, err.Error(), "total docs examined surpassed threashold")
@@ -54,15 +30,33 @@ func TestDebugFindWithoutIndex(t *testing.T) {
 
 func TestDebugFindWithIndex(t *testing.T) {
 	ctx := context.Background()
+	client, chosenUserName := setup(t, true)
+
+	cursor, err := mongodebug.
+		Debug(mongodebug.WithThreshold(10)).
+		WithDatabase(client.Database("db")).
+		WithCollection("collection").
+		Find(ctx, bson.D{{"name", chosenUserName}})
+
+	require.Nil(t, err)
+	require.NotNil(t, cursor)
+}
+
+func setup(t *testing.T, withIndex bool) (*mongo.Client, string) {
+	ctx := context.Background()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/db"))
 	require.NoError(t, err)
 
 	err = client.Database("db").CreateCollection(ctx, "collection")
 	require.NoError(t, err)
 
-	client.Database("db").Collection("collection").Indexes().CreateOne(ctx, mongo.IndexModel{
-		Keys: bson.M{"name": 1},
-	})
+	if withIndex {
+		_, err = client.Database("db").Collection("collection").Indexes().CreateOne(ctx, mongo.IndexModel{
+			Keys: bson.M{"name": 1},
+		})
+
+		require.NoError(t, err)
+	}
 
 	faker := faker.New()
 
@@ -84,12 +78,5 @@ func TestDebugFindWithIndex(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	cursor, err := mongodebug.
-		Debug(mongodebug.WithThreshold(10)).
-		WithDatabase(client.Database("db")).
-		WithCollection("collection").
-		Find(ctx, bson.D{{"name", name}})
-
-	require.NotNil(t, cursor)
-	require.Nil(t, err)
+	return client, name
 }
